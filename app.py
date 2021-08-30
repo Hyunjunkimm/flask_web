8c2a511155684cf565558bd13092f14635febf3e
hyunjun kim <kimhj2a@Naver.com>
2021-08-30 15:22:12 +0900

    articles.html step1

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask , render_template
+from data import Articles
 
 app = Flask(__name__)
 
+@app.route('/hello')
+def hello_world():
+    return 'Hello World!'
 
-@app.route('hello')
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    name = "Kim"
+    return render_template('index.html' , data = name)
+
+@app.route('/articles', methods=['GET', 'POST'])
+def articles():
+    list_data = Articles()
+    return render_template('articles.html', data = list_data)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
